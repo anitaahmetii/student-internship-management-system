5b3463855f144b3e49559cde427f959a9f7c7fa0
Anita Ahmeti <aniita.ahmeti@gmail.com>
2026-02-10 08:30:35 +0100

    Add admin-only access control for location endpoints

--- routes/city.route.js
+++ routes/city.route.js
@@ -2,11 +2,12 @@ const express = require('express');
 const route = express.Router();
 const cityController = require('../controllers/city.controller');
 const validate = require('../middleware/validators');
+const auth = require('../middleware/auth');
 
-route.post('/add', validate.validateCity, cityController.createCity);
+route.post('/add', auth.verifyToken(['admin']), validate.validateCity, cityController.createCity);
 route.get('/', cityController.getCities);
-route.put('/update/:city/:state', validate.validateToUpdateCity, cityController.updateCity);
-route.delete('/delete/:city/:state', cityController.deleteCity);
+route.put('/update/:city/:state', auth.verifyToken(['admin']), validate.validateToUpdateCity, cityController.updateCity);
+route.delete('/delete/:city/:state', auth.verifyToken(['admin']), cityController.deleteCity);
 route.get('/cities/:state', cityController.findCitiesByState);
 route.get('/state/:city', cityController.findStateByCity);
 
--- routes/state.route.js
+++ routes/state.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const route = express.Router();
 const stateController = require('../controllers/state.controller');
 const validator = require('../middleware/validators');
+const auth = require('../middleware/auth');
 
-route.post('/add', validator.validateState, stateController.createState);
+route.post('/add', auth.verifyToken(['admin']), validator.validateState, stateController.createState);
 route.get('/', stateController.getStates);
-route.put('/update/:name', validator.validateToUpdateState, stateController.updateState);
-route.delete('/delete/:name', stateController.deleteState);
+route.put('/update/:name', auth.verifyToken(['admin']), validator.validateToUpdateState, stateController.updateState);
+route.delete('/delete/:name', auth.verifyToken(['admin']), stateController.deleteState);
 
 module.exports = route;
